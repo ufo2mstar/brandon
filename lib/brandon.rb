07c2307d14ad60639264db5da8c4a334a8b68b80
Naren SivaSubramani <ufo2mstar@gmail.com>
2018-02-04 00:32:39 -0500

    SPEC: solid foundation, coverage checked and added complete edge cases

--- lib/brandon.rb
+++ lib/brandon.rb
@@ -6,7 +6,7 @@ class Brandon
   attr_accessor :root_dir, :tree_hash
 
   def initialize root_dir, tree_hash
-    @root_dir = root_dir
+    @root_dir = root_dir || "."
     @paths = []
     build_dir_structure tree_hash
   end
@@ -14,23 +14,23 @@ class Brandon
   def build
     # yp @paths
     dirs =[]; files=[]
-    @paths.each {|itm| (itm[-1] == "/" ? dirs : files) << itm}
+    paths.each {|itm| (itm[-1] == "/" ? dirs : files) << itm}
     # yp @dirs; yp @files
     dirs.each {|itm| make_dir itm}
     files.each {|itm| make_file itm}
-    # @paths
+    paths
   end
 
   private
   def build_dir_structure struct
-    cycle_hsh struct, "#{root_dir}/"
-    @paths.map {|k| k.gsub(" ", "_")} # snake_caseize_all_names!
+    cycle_hsh struct, "#{root_dir}#{"/" if root_dir[-1] != "/" }"
+    paths.map {|k| k.gsub(" ", "_")} # snake_caseize_all_names!
   end
 
   def cycle_hsh hsh, location
     hsh.each {|k, v|
-      loc = "#{location}#{k}/"
-      @paths << loc
+      loc = "#{location}#{k}/" #making it a dir
+      paths << loc
       cls=v.class.to_s
       if cls == "Hash"
         cycle_hsh v, loc
@@ -50,14 +50,17 @@ class Brandon
         when "Hash" then
           cycle_hsh itm, loc
         else
-          @paths << loc+itm
+          cycle_other itm, loc
       end
     }
   end
 
   def cycle_other itm, loc
-    if itm.class == String and itm.size > 0
-      @paths << loc+itm
+    # if itm.class == String and !itm.strip.empty?
+    # supporting any non-blank types!
+    itm = itm.to_s
+    if !itm.strip.empty?
+      paths << loc+itm # making the file
     else
       # skip if Nil or blank string!
     end
--- lib/brandon_spec.rb
+++ lib/brandon_spec.rb
@@ -2,24 +2,49 @@ require 'rspec'
 require_relative 'brandon'
 
 describe Brandon do
+  let(:curr_dir) {File.dirname(__FILE__)}
+  let(:root_dir) {"."}
 
-  context "tree build" do
-    describe "tree builder" do
-      let(:tree_hash) {{main1: {sub1: nil, sub2: "sub2file1.txt", sub3: {sub4: ""}}, main2: ""}}
-      let(:root_dir) {File.dirname(__FILE__)}
-      # let(:root_dir) {""}
+  def build_subject tree, dir = root_dir
+    Brandon.new dir, tree
+  end
+
+  def build_res ary, dir = root_dir
+    ary.map {|loc| "#{dir}#{loc}"}
+  end
+
+  context "tree parse" do
+    describe "should take the yml and store the dirs and files correctly for" do
+      let(:tree_hash) {{main1: {sub1: nil, sub2: "sub2file1.txt", sub3: {sub4: "  ", sub5: '', :sub6 => :kod, :sub7 => 2342}}, main2: ""}}
+      let(:file_hash) {{main1: [nil, "", "  ", "str.file", 1234, :symbol, Object, Hash.new]}}
+      let(:file_edgecase_hash) {{main2: {sub1: [:sub2 => [[:one, 'two'], 3], :sub3 => 2342]}}}
+
       subject {Brandon.new root_dir, tree_hash}
 
-      it "should take the yml and store the dirs and files correctly" do
-        bran = build_at()
-        res = %w[/main1/ /main1/sub1/ /main1/sub2/ /main1/sub2/sub2file1.txt /main1/sub3/ /main1/sub3/sub4/ /main2/].map {|loc| "#{root_dir}#{loc}"}
-        expect(subject.paths).to eq(res)
+      it "any type of file from ary" do
+        tree = file_hash
+        bran = build_subject(tree)
+        # res = build_res %w[]
+        res = build_res ["./main1/", "./main1/str.file", "./main1/1234", "./main1/symbol", "./main1/Object"], ""
+
+        expect(bran.paths).to eq(res)
       end
 
-      it "should default location to current dir" do
+      it "even malformed edge cases" do
+        tree = file_edgecase_hash
+        bran = build_subject(tree)
+        res = build_res ["./main2/", "./main2/sub1/", "./main2/sub1/sub2/", "./main2/sub1/sub2/one", "./main2/sub1/sub2/two", "./main2/sub1/sub2/3", "./main2/sub1/sub3/", "./main2/sub1/sub3/2342"], ""
 
+        expect(bran.paths).to eq(res)
       end
+    end
+  end
 
+  context "builder" do
+
+    it "should default location to current dir" do
+      bran = build_subject({}, nil)
+      expect(bran.root_dir).to eq(".")
     end
   end
 
