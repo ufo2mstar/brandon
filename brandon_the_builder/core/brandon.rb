bbf8808090f4146cb5ab2ec8708194904e6f2fd6
Naren Siva Subramani <narensub@amazon.com>
2020-12-27 00:05:10 -0600

    WIP: Successful erb template create

--- brandon_the_builder/core/brandon.rb
+++ brandon_the_builder/core/brandon.rb
@@ -2,6 +2,8 @@ require 'ap'
 require 'yaml'
 
 module BrandonSupport
+  OverwriteError = Class.new Interrupt
+
   # Just checks to see if root directory already has a *bran* trace
   # @return [Nil]
   def check_if_overwriting loc
@@ -21,41 +23,54 @@ end
 module BrandonSubs
   def sub_names content_path, sub_names_hash
     res = content_path
+    #todo: will improve hack which might potentially overwrite a sub-matching pattern ie: PATTERN overwrites PATTERN_NEW
     sub_names_hash.each { |from, to| res = res.gsub(from, to) }
     res
   end
 
-  def sub_contents content_file_path, sub_contents_hash
-    ERB.new(File.read(content_file_path)).result_with_hash sub_contents_hash
+  def sub_contents file_content, sub_contents_hash
+    ERB.new(file_content).result_with_hash sub_contents_hash
   end
 end
 
 class BrandonBuilder
   attr_accessor :template_map, :dir_map
   include BrandonSupport
+  include BrandonSubs
 
   BRANDON_TEMPLATE_MAPS = 'brandon_maps.yml'
+  NAMES = 'names'
+  CONTENTS = 'contents'
 
   def initialize(template_source_path, template_maps_file = BRANDON_TEMPLATE_MAPS)
     @template_source = template_source_path
     @template_map = read_brandon_map(template_source_path, template_maps_file)
-    @dir_map = Dir["#{template_source_path}/**/*"]
+    @dir_map = Dir["#{template_source_path}/**/*"].reject { |f| f =~ /#{BRANDON_TEMPLATE_MAPS}/ }
   end
 
   def create(dest_path)
-    ap dest_path
+    source_dir = File.dirname(@template_source)
+
+    # create base dest path
+    dest_unsubbed = @template_source.gsub(source_dir, '')
+    subbed_source = sub_names dest_unsubbed, @template_map[NAMES]
+    dest = File.join(dest_path, subbed_source)
+    make_dir dest
+
     @dir_map.each do |source|
-      subbed_source = sub_names @template_source, @template_map.names
-      dest = source.gsub(subbed_source, dest_path)
+      dest_unsubbed = source.gsub(source_dir, '')
+      subbed_source = sub_names dest_unsubbed, @template_map[NAMES]
+      dest = File.join(dest_path, subbed_source)
       if File.directory? source
-        ap "make_dir #{dest}"
-        # make_dir dest
+        # ap "make_dir #{dest}"
+        check_if_overwriting(dest)
+        make_dir dest
       else # File.file? path
-        ap "fill_file #{dest}, #{source}"
+        # ap "fill_file #{dest}, #{source}"
         fill_file dest, source
       end
     end
-    ap 'done'
+    ap "done writing to #{dest_path}"
   end
 
   private
@@ -65,17 +80,19 @@ class BrandonBuilder
   end
 
   def fill_file dest, source
-    ap "make_file #{dest}"
-    # make_file dest
-    new_contents = sub_contents source, @template_map.contents
-    ap new_contents
-    # File.open(dest, 'w') { |f| f.puts new_contents }
+    ext = File.extname(source)
+    dest = ext =~ /.erb$/ ? dest.gsub(/\.erb$/, '') : dest
+    make_file dest
+    file_content = File.read(source)
+    new_contents = ext == '.erb' ? sub_contents(file_content, @template_map[CONTENTS]) : file_content
+    # ap new_contents
+    File.open(dest, 'w') { |f| f.puts new_contents }
   end
 
   def get_page_structure
     erb = ERB.new(File.read(BRANDON_TEMPLATE_MAPS)).result binding
     yp erb
-    hsh = YAML.load erb.gsub("=>", ": ")
+    hsh = YAML.load erb.gsub('=>', ': ')
     # yp hsh
   end
 end
--- brandon_the_builder/spec/brandon_spec.rb
+++ brandon_the_builder/spec/brandon_spec.rb
@@ -3,9 +3,14 @@ require 'rspec/snapshot'
 
 describe 'BrandonBuilder' do
   THIS_DIR = File.dirname(__FILE__)
+  DATA_DIR = File.join(THIS_DIR, 'data')
+  DEST_DIR = File.join(DATA_DIR, 'temp')
+
+  FileUtils::rm_rf DEST_DIR
+
   context 'init' do
     it 'should read root path by default' do
-      root = File.join(THIS_DIR, 'data', 'DEMO_SCRIPT_TEMPLATE')
+      root = File.join(DATA_DIR, 'DEMO_SCRIPT_TEMPLATE')
       bran = BrandonBuilder.new root
       res = bran.template_map
 
@@ -13,7 +18,7 @@ describe 'BrandonBuilder' do
     end
 
     it 'should read data non-derfault' do
-      root = File.join(THIS_DIR, 'data', 'DEMO_SCRIPT_TEMPLATE')
+      root = File.join(DATA_DIR, 'DEMO_SCRIPT_TEMPLATE')
       bran = BrandonBuilder.new root, 'brandon_maps.yml'
       res = bran.template_map
 
@@ -23,13 +28,37 @@ describe 'BrandonBuilder' do
 
   context 'walk' do
     it 'should read dir files' do
-      root = File.join(THIS_DIR, 'data', 'DEMO_SCRIPT_TEMPLATE')
+      root = File.join(DATA_DIR, 'DEMO_SCRIPT_TEMPLATE')
       bran = BrandonBuilder.new root
 
       base = File.basename(root)
       dir = File.dirname(root)
-      res = bran.dir_map.map{|s| s.gsub(dir,'')}
+      res = bran.dir_map.map { |s| s.gsub(dir, '') }
       expect(res).to match_snapshot 'demo_walk'
+    end
+  end
+
+  context 'sub' do
+    it 'should create default' do
+      source = File.join(DATA_DIR, 'DEMO_SCRIPT_TEMPLATE')
+      dest = DEST_DIR
+      bran = BrandonBuilder.new source
+
+      base = File.basename(source)
+      dir = File.dirname(source)
+      res = bran.create dest
+      # expect(res).to match_snapshot 'demo_walk'
+    end
+
+    it 'should create sample' do
+      source = File.join(DATA_DIR, 'DEMO_SCRIPT_TEMPLATE')
+      dest = DEST_DIR
+      bran = BrandonBuilder.new source, 'sample_brandon_maps.yml'
+
+      base = File.basename(source)
+      dir = File.dirname(source)
+      res = bran.create dest
+      # expect(res).to match_snapshot 'demo_walk'
     end
   end
 end
